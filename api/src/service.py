430a2fa45922524275562409c7edfdaca11143b8
Maxym Shyrchenko <max1999shyrchenko@gmail.com>
2024-09-17 23:17:40 +0300

    Work with different warehouses

--- api/src/service.py
+++ api/src/service.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from typing import List
 
 from models import TaskData, WarehouseMapping, OutputModel
@@ -8,27 +9,48 @@ from db import create_session
 class WarehouseSelectorService:
     ACCEPTABLE_TIME_OF_EXECUTION = 20
     COEFFICIENTS = [0.1, 0.15, 0.15, 0.2, 0.4]
+    WAREHOUSES_RATE = [
+        WarehouseMapping.SMALL,
+        WarehouseMapping.MEDIUM,
+        WarehouseMapping.LARGE,
+        WarehouseMapping.XLARGE
+    ]
 
     def __init__(self):
         self._execution_statistics = ExecutionStatisticsRepository(
             session=create_session()
         )
 
+    def _choose_warehouse(self, fraction: float, major_warehouse: WarehouseMapping) -> WarehouseMapping:
+        warehouse_index = self.WAREHOUSES_RATE.index(major_warehouse)
+        if fraction < 0.5:
+            new_warehouse_index = warehouse_index - 1
+        elif fraction < 1:
+            new_warehouse_index = warehouse_index
+        elif fraction < 3:
+            new_warehouse_index = warehouse_index + 1
+        elif fraction < 10:
+            new_warehouse_index = warehouse_index + 2
+        else:
+            new_warehouse_index = warehouse_index + 3
+        new_warehouse_index = min(len(self.WAREHOUSES_RATE), new_warehouse_index)
+        new_warehouse_index = max(0, new_warehouse_index)
+        return self.WAREHOUSES_RATE[new_warehouse_index]
+
     def _get_warehouse_type(self, task_data: TaskData) -> WarehouseMapping:
-        last_executions = self._execution_statistics.get_last_n_executions(task_data.task_name, 5)
+        last_executions = self._execution_statistics.get_last_n_executions(
+            task_data.task_name,
+            len(self.COEFFICIENTS)
+        )
         durations = [statistic.duration for statistic in last_executions]
         average = sum(map(lambda mapping: mapping[0] * mapping[1], zip(durations, self.COEFFICIENTS)))
 
         fraction = average / self.ACCEPTABLE_TIME_OF_EXECUTION
 
-        if fraction < 1:
-            return WarehouseMapping.SMALL
-        elif fraction < 3:
-            return WarehouseMapping.MEDIUM
-        elif fraction < 10:
-            return WarehouseMapping.LARGE
-        else:
-            return WarehouseMapping.XLARGE
+        major_warehouse_type = Counter(
+            [statistic.warehouse_size for statistic in last_executions]
+        ).most_common(1)[0][0]
+        return self._choose_warehouse(fraction, major_warehouse_type)
 
     def process_request(self, payload: List[TaskData]) -> List[OutputModel]:
         responses = []
